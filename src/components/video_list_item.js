59d26f936d872d5f3e5f00a98fc4e3cec8c8252e
Doris <DorisMac@doriss-mbp.speedport_w_723v_1_45_000>
2017-11-18 15:42:13 +0100

    Initial Commit

---
+++ src/components/video_list_item.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+const VideoListItem = (props) => {
+
+  const video = props.video;
+
+  const ImageURL = video.snippet.thumbnails.default.url;
+  const TitleURL = video.snippet.title;
+
+  return(
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={ImageURL}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading"> {TitleURL} </div>
+        </div>
+      </div>
+    </li>
+
+  );
+};
+
+export default VideoListItem;
